89219050796a9639805f6d4a186466b21fe27809
Cuma Cakmak <cumacakmak@gmail.com>
2023-04-06 00:18:30 +0300

    Update definition

--- 226.invert-binary-tree.java
+++ 226.invert-binary-tree.java
@@ -1,25 +1,25 @@
 /*
- * @lc app=leetcode id=226 lang=java
+@lc app=leetcode id=226 lang=java
  *
- * [226] Invert Binary Tree
+[226] Invert Binary Tree
  */
 
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+ 
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root==null) return null;
