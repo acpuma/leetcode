756845714a9fb50a566e7c4d4ad29b1eafe50b32
Cuma Cakmak <cumacakmak@gmail.com>
2023-04-07 21:52:06 +0300

    [98] Validate Binary Search Tree

---
+++ 98.validate-binary-search-tree.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=98 lang=java
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValid(TreeNode root, long min, long max) {
+        if (root==null) return true;
+        if (root.val >= max || root.val <= min) return false;
+        return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
+    }
+}
+// @lc code=end
+
