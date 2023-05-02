a63b9cd3983c794ab7f5ae3e8772bc2fa1183a06
Cuma Cakmak <cumacakmak@gmail.com>
2023-05-02 19:41:27 +0300

    [1779] Find Nearest Point That Has the Same X or Y Coordinate

---
+++ 1779.find-nearest-point-that-has-the-same-x-or-y-coordinate.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=1779 lang=java
+ *
+ * [1779] Find Nearest Point That Has the Same X or Y Coordinate
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int nearestValidPoint(int x, int y, int[][] points) {
+        int min=Integer.MAX_VALUE;
+        int index=-1;
+        for (int i=0;i<points.length; i++) {
+            if (points[i][0]==x || points[i][1]==y) {
+                int des = Math.abs(x-points[i][0])+Math.abs(y-points[i][1]);
+                if (des<min) {
+                    min=des;
+                    index=i;
+                }
+            }
+        }
+        return index;
+    }
+}
+// @lc code=end
+
