6dbad827492ddd3c3e4c55a979d8380de0158bda
Cuma Cakmak <cumacakmak@gmail.com>
2023-05-07 21:57:39 +0300

    [1678] Goal Parser Interpretation

---
+++ 1678.goal-parser-interpretation.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1678 lang=java
+ *
+ * [1678] Goal Parser Interpretation
+ */
+
+// @lc code=start
+class Solution {
+    public String interpret(String command) {
+        return command.replace("()", "o")
+            .replace("(al)", "al");
+    }
+}
+// @lc code=end
+
