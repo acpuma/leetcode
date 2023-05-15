89e4a0372f2f8345b42d9d03a69ab6f00d9ce291
Cuma Cakmak <cumacakmak@gmail.com>
2023-05-15 19:54:36 +0300

    [994] Rotting Oranges - BFS

--- 994.rotting-oranges.java
+++ 994.rotting-oranges.java
@@ -6,41 +6,47 @@
 
 // @lc code=start
 
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
     public int orangesRotting(int[][] grid) {
-        //DFS
-        if (grid==null || grid.length==0) return -1;
-        for (int i=0; i<grid.length; i++) {
-            for (int j=0; j<grid[0].length; j++) {
-                if (grid[i][j]==2)
-                    rot(grid, i, j, 2);
+        //BFS
+        if (grid==null || grid.length==0) return 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int count_fresh =0 ;
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                if (grid[i][j]==2) {
+                    queue.offer(new int[]{i,j});
+                } else if (grid[i][j]==1) {
+                    count_fresh++;
+                }
             }
         }
-        int minutes=2;
-        for (int[] row:grid) {
-            for (int cell:row) {
-                if (cell==1) return -1;
-                minutes = Math.max(minutes,cell);
+        if (count_fresh==0) return 0;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        int count=0;
+        while (!queue.isEmpty()) {
+            ++count;
+            int size = queue.size();
+            for (int i=0; i<size; i++) {
+                int[] point =queue.poll();
+                for (int[] dir:dirs) {
+                    int x= point[0]+dir[0];
+                    int y= point[1]+dir[1];
+                    if (x<0 || y <0 || x>=rows || y>=cols 
+                            || grid[x][y] == 0 || grid[x][y]==2) 
+                        continue;
+                    grid[x][y]=2;
+                    queue.offer(new int[]{x,y});
+                    count_fresh--;
+                }
             }
         }
-        return minutes-2;
-    }
-
-    private void rot(int[][] grid, int i, int j, int minutes) {
-        if (i<0 || i>=grid.length
-                || j<0 || j>=grid[0].length
-                || grid[i][j]==0
-                || (1<grid[i][j] && grid[i][j]<minutes))
-            return ;
-        else {
-            grid[i][j]=minutes;
-            rot(grid, i-1, j, minutes+1);
-            rot(grid, i+1, j, minutes+1);
-            rot(grid, i, j-1, minutes+1);
-            rot(grid, i, j+1, minutes+1);
-        }
+        return count_fresh == 0 ? count-1 : -1;
     }
 }
 // @lc code=end
