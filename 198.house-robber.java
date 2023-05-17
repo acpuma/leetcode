78519d347931f077ca37ac99363d1af2ac69ee82
Cuma Cakmak <cumacakmak@gmail.com>
2023-05-17 14:52:13 +0300

    [198] House Robber

---
+++ 198.house-robber.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=198 lang=java
+ *
+ * [198] House Robber
+ */
+
+// @lc code=start
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        int[] memo = new int[nums.length+1];
+        memo[0]=0;
+        memo[1]=nums[0];
+        for (int i=1; i<nums.length; i++) {
+            int val = nums[i];
+            memo[i+1] = Math.max(memo[i], memo[i-1]+val);
+        }
+        return memo[nums.length];
+    }
+}
+// @lc code=end
+
